3d32000dc69d219ab1e8cae0386e29b5c36a9a0e
谢 <921035044@qq.com>
2017-04-17 22:25:31 +0800

    angular---route/ui/ng

---
+++ dierzhou/js/base.js
@@ -0,0 +1,11 @@
+var app = angular.module('demo',['ui.router']);
+app.config(function($stateProvider){
+	$stateProvider.state('nav',{
+		url:'/nav',
+		templateUrl:'directive/nav.html'
+	})
+	// .state('nav.a',{
+	// 	url:'/a',
+	// 	templateUrl:'directive/asideNav.html'
+	// })
+})
